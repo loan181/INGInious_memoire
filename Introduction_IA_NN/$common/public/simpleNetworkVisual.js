85c911200aab62bf05f98d75ee55a685d9875ce9
loan181 <loan181@hotmail.fr>
2019-07-18 12:04:49 +0200

    draw neural network

--- Introduction_IA_NN/$common/public/simpleNetworkVisual.js
+++ Introduction_IA_NN/$common/public/simpleNetworkVisual.js
@@ -32,6 +32,47 @@ function createNeuralNetwork() {
     let nnDiv = document.createElement("div");
     nnDiv.id = "raphaelNN";
     visu.appendChild(nnDiv);
+
+    let paperSize = 120;
+    let paper = Raphael(nnDiv, paperSize, paperSize);
+
+    const layersNeuronsNumber = [9, 3, 2];
+    const neuronSize = 10;
+
+    let saveXYPos = [];
+    for (let _ = 0; _ < layersNeuronsNumber.length; _++) {
+        saveXYPos.push([]);
+    }
+
+
+    const neuronRadius = neuronSize/2;
+    let paperHorizontalSpace = paperSize - neuronSize;
+    let horizontalOffset = paperHorizontalSpace/(layersNeuronsNumber.length-1);
+    for (let i = 0; i < layersNeuronsNumber.length; i++) {
+        let x = neuronRadius + i*horizontalOffset;
+        let verticalOffset = paperSize/(layersNeuronsNumber[i]+1);
+        for (let j = 0; j < layersNeuronsNumber[i]; j++) {
+            let y = (j+1)*verticalOffset;
+            paper.circle(x, y, neuronRadius)
+            .attr({fill: "white", stroke:"#AAA"});
+
+            saveXYPos[i].push([x, y]);
+        }
+    }
+
+    for (let i = 0; i < saveXYPos.length-1; i++) {
+        for (let j = 0; j < saveXYPos[i].length; j++) {
+            for (let k = 0; k < saveXYPos[i + 1].length; k++) {
+                let x1 = saveXYPos[i][j][0];
+                let y1 = saveXYPos[i][j][1];
+                let x2 = saveXYPos[i+1][k][0];
+                let y2 = saveXYPos[i+1][k][1];
+
+                let line = paper.path(`M${x1} ${y1}L${x2} ${y2}`)
+                    .toBack();
+            }
+        }
+    }
 }
 
 create3x3InteractiveGrid();
