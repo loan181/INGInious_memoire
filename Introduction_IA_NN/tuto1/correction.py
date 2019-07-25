438765aaa07f3c5492c45234bc9ed4dc4856701f
loan181 <loan181@hotmail.fr>
2019-07-26 00:09:14 +0200

    update task Blockly toolbar and create second task

--- Introduction_IA_NN/tuto1/correction.py
+++ Introduction_IA_NN/tuto1/correction.py
@@ -36,8 +36,7 @@ def handleInputLayer(image):
             inc += 1
 
 def matmult(a,b):
-    zip_b = zip(*b)
-    zip_b = list(zip_b)
+    zip_b = list(zip(*b))
     return [[sum(ele_a*ele_b for ele_a, ele_b in zip(row_a, col_b))
              for col_b in zip_b] for row_a in a]
 
